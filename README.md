55d6f752779dd6f430923fe85f3ea20ee2c32a02
aconamos <nephi.norton@gmail.com>
2020-09-08 23:20:17 -0600

    functional

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # twitch chat rewards clicker
  made so i can lay back on my chair and watch streams without having to click the green +50 points button all the time and still get points, figured id share
+
+ also only works on 1080p monitors
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import cv2
+import pyautogui
+
+from time import sleep
 
 
+startx = 1659
+starty = 1003
+endx = 1691
+endy = 1025
 
 def get_image():
-    img = cv2.
+    return pyautogui.screenshot(region=(startx, starty, endx, endy))
+
+
+while True:
+    pixs = get_image().getpixel((1, 1))
+    if pixs[0] == 0 and pixs[1] == 230 and pixs[2] == 203:
+        pyautogui.click(startx, starty)
+    sleep(3)
+    
